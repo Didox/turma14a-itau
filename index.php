55820099389f460de7e3962738e3e698a1cf5b20
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-04 14:11:03 -0300

    colocando um quadrado no html

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 
 <h5>Tentar sempre, desistir jamais!!!<h5>
   
-<h7>testando!!!<h7>
-<h10>Tentar sempre, desistir as vezes!!!<h10>
-<h7>QUERO DELETAR TUDO </h7>
+<h1>testando!!!<h1>
+<h1>Tentar sempre, desistir as vezes!!!<h1>
+<h1>QUERO DELETAR TUDO </h1>
+
+<div style="width:200px;height:200px;background:yellow">
+  Um teste em um quadrado
+</div>
