9112984ce7d3682a71c6696a9a92e962ac7541f0
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-04 15:32:07 -0300

    colocando um quadrado no html

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 <h3> Um deploy aotomático feito com alunos do itaú </h3>
 
 <div style="display:flex">
-  <div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
+  <div style="width:200px;height:400px;background:yellow;padding:50px;margin:10px">
     Um teste em um quadrado
     <br>
     <img src="b1.jpg" style="width: 200px">
+    <br><br>
+    R$ 550,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
   </div>
 
-  <div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+  <div style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
     Um teste em um quadrado
     <br>
     <img src="b2.jpg" style="width: 200px">
+   <br><br>
+    R$ 850,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
   </div>
 </div>
